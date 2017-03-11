111ae539e8b169899f8661e56890cbb0b3f97456
SuperStephenHaze <thatfoolishgamer@gmail.com>
2017-03-11 13:04:05 +0000

    everything needed
    everything needed for project

---
+++ Roll a Ball/New Unity Project/Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControler : MonoBehaviour {
+
+    public float speed;
+
+    private Rigidbody rb;
+
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        float moveVerticle = Input.GetAxis ("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVerticle);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pick UP"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Roll a Ball/New Unity Project/Assets/Scripts/Rotator.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour {
+
+	void Update () // Update is called once per frame
+    {
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+}
